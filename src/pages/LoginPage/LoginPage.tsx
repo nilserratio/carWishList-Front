348c80aa996d9ccfd4c236c9f3ef5d27ecd6d661
Nil Serra Tió <nilserratio@gmail.com>
2023-10-11 16:07:36 +0200

    Create LoginPage page component

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,11 @@
+import LoginPageStyled from "./LoginPageStyled";
+
+const LoginPage = (): React.ReactElement => {
+  return (
+    <LoginPageStyled className="login-container">
+      <h1 className="login-container__title">Sign in</h1>
+    </LoginPageStyled>
+  );
+};
+
+export default LoginPage;
