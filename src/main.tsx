5a12939aec3be4ed7f03cdec62095c30d394e23b
Nil Serra Tió <nilserratio@gmail.com>
2023-10-11 11:15:33 +0200

    Create appRouter rendering App on '/' path

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
 import "@fontsource/poppins";
-import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { ThemeProvider } from "styled-components";
+import { RouterProvider } from "react-router-dom";
 import theme from "./styles/theme/theme";
 import GlobalStyle from "./styles/GlobalStyles/GlobalStyles";
+import appRouter from "./routers/appRouter";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Provider store={store}>
-        <App />
+        <RouterProvider router={appRouter} />
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
