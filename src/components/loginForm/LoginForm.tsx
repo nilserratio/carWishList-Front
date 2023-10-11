9792c1cf5152d96f268340e1a267e62358863db4
Nil Serra Tió <nilserratio@gmail.com>
2023-10-11 17:35:51 +0200

    Add state variable on LoginForm component

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
+import { UserCredentials } from "../../types";
 import LoginFormStyled from "./LoginFormStyled";
 
 const LoginForm = (): React.ReactElement => {
+  const initialUserCredentials: UserCredentials = {
+    username: "",
+    password: "",
+  };
+
+  const [userCredentials, setUserCredentials] = useState(
+    initialUserCredentials
+  );
+
+  const onChangeData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserCredentials({
+      ...userCredentials,
+      [event.target.id]: event.target.value,
+    });
+  };
+
   return (
     <LoginFormStyled className="loginForm-container" autoComplete="off">
       <div className="login-container__control">
         <label htmlFor="username">Username or userID</label>
-        <input id="username" type="text"></input>
+        <input id="username" type="text" onChange={onChangeData}></input>
       </div>
 
       <div className="login-container__control">
         <label htmlFor="password">Password</label>
-        <input id="password" type="password"></input>
+        <input id="password" type="password" onChange={onChangeData}></input>
       </div>
 
       <button className="login-container__submit">Sign in</button>
