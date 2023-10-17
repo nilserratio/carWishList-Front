a3977b8f166fae01d87f793e5d6051937436f593
Nil Serra Tió <nilserratio@gmail.com>
2023-10-17 11:27:52 +0200

    Add setToken function test

---
+++ src/hooks/localStorage/__tests__/setToken.test.ts
@@ -0,0 +1,22 @@
+import { renderHook } from "@testing-library/react";
+import { tokenMock } from "../../../mocks/user/userMocks";
+import useLocalStorage from "../useLocalStorage";
+
+describe("Given a useLoaclStorage custom hook with setToken function", () => {
+  describe("When the function getToken is called with a key 'token' and the value", () => {
+    test("Then it should save the token in the local storage", () => {
+      const key = "token";
+      const value = tokenMock;
+
+      const {
+        result: {
+          current: { setToken },
+        },
+      } = renderHook(() => useLocalStorage());
+
+      setToken(key, value);
+
+      expect(localStorage.getItem(key)).toBe(value);
+    });
+  });
+});
