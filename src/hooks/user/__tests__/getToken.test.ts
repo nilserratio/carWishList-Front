f0efbcf322b2da5915fdd460842056f7364c6362
Nil Serra Tió <nilserratio@gmail.com>
2023-10-17 12:23:05 +0200

    Add error management getToken fucntion test on useUser custom hook

--- src/hooks/user/__tests__/getToken.test.ts
+++ src/hooks/user/__tests__/getToken.test.ts
@@ -2,11 +2,15 @@ import { renderHook } from "@testing-library/react";
 import useUser from "../useUser";
 import { loginUserDataMock } from "../../../mocks/user/userMocks";
 import { tokenMock } from "../../../mocks/user/userMocks";
+import { server } from "../../../mocks/server";
+import { errorHandlers } from "../../../mocks/handlers";
+import { UserCredentials } from "../../../types";
 
 describe("Given a useUser custom hook with getToken function", () => {
+  const user: UserCredentials = loginUserDataMock;
+
   describe("When the function getToken is called with a username a and password", () => {
     test("Then it should return a token", async () => {
-      const user = loginUserDataMock;
       const expectedToken = tokenMock;
 
       const {
@@ -18,6 +22,22 @@ describe("Given a useUser custom hook with getToken function", () => {
       const token = await getToken(user);
 
       expect(token).toBe(expectedToken);
+    });
+  });
+
+  describe("When the function getToken is called with an invalid user credentials", () => {
+    test("Then it should return the response's method with an error with 401 status code", () => {
+      server.resetHandlers(...errorHandlers);
+
+      const {
+        result: {
+          current: { getToken },
+        },
+      } = renderHook(() => useUser());
+
+      const getTokenFunction = getToken(user);
+
+      expect(getTokenFunction).rejects.toThrowError();
     });
   });
 });
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -9,3 +9,9 @@ export const handlers = [
     return res(ctx.status(200), ctx.json({ token: tokenMock }));
   }),
 ];
+
+export const errorHandlers = [
+  rest.post(`${apiUrl}${paths.user}${paths.login}`, (_req, res, ctx) => {
+    return res(ctx.status(401));
+  }),
+];
