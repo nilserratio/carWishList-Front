c3a15a22c380b60dfebfdf6df01d88fcb3620c73
Nil Serra Tió <nilserratio@gmail.com>
2023-10-18 10:07:59 +0200

    Add dispatch loadCars to HomePage

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import { useAppDispatch } from "../../store";
 import HomepageStyled from "./HomepageStyled";
+import { loadCarsActionCreator } from "../../store/cars/carsSlice";
+import { carsMock } from "../../mocks/cars/carsMocks";
 
 const Homepage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(loadCarsActionCreator(carsMock));
+  }, [dispatch]);
+
   return (
     <HomepageStyled className="homepage-container">
       <h1 className="homepage-container__title">Recomotor favorite cars</h1>
