8123b886e4d2190cf5b8b4cdb91353b9247d43d2
Nil Serra Tió <nilserratio@gmail.com>
2023-10-18 10:25:36 +0200

    Create CarsListStyled styles

---
+++ src/components/CarsList/CarsListStyled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const CarsListStyled = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 28px;
+`;
+
+export default CarsListStyled;
