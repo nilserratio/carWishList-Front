6a5a922b231fc408ffbc611511451b05124150be
Nil Serra Tió <nilserratio@gmail.com>
2023-10-19 09:49:45 +0200

    Add getCars function test

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -9,7 +9,7 @@ export const handlers = [
   rest.post(`${apiUrl}${paths.user}${paths.login}`, (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ token: tokenMock }));
   }),
-  rest.get(`${apiUrl}${paths.home}`, (_req, res, ctx) => {
+  rest.get(`${apiUrl}${paths.cars}`, (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ cars: carsMock }));
   }),
 ];
