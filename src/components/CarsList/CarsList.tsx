400ef71fb71d855ce5896bd84f9538b86c02a7ba
Nil Serra Tió <nilserratio@gmail.com>
2023-10-24 21:12:15 +0200

    Add if a car is favorite on CarList component

--- src/components/CarsList/CarsList.tsx
+++ src/components/CarsList/CarsList.tsx
@@ -4,6 +4,11 @@ import CarsListStyled from "./CarsListStyled";
 
 const CarsList = (): React.ReactElement => {
   const marcas = useAppSelector((state) => state.cars.marcas);
+  const { favoriteCars } = useAppSelector((state) => state.user);
+
+  const isModelInFavorites = (modelId: string) => {
+    return favoriteCars.some((favoriteModelId) => favoriteModelId === modelId);
+  };
 
   return (
     <CarsListStyled className="carsList-container" aria-label="list of cars">
@@ -13,7 +18,11 @@ const CarsList = (): React.ReactElement => {
           <ul className="carsList-container__model-list">
             {marca.modelos.map((modelo) => (
               <li key={modelo.id} className="carsList-container__model-item">
-                <CarCard modelo={modelo} marca={marca} />
+                <CarCard
+                  modelo={modelo}
+                  marca={marca}
+                  isFavorite={isModelInFavorites(modelo.id)}
+                />
               </li>
             ))}
           </ul>
