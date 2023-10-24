1c2eba5d7920501ca1c6d6a36987b632c67ea208
Nil Serra Tió <nilserratio@gmail.com>
2023-10-24 17:03:39 +0200

    Change user mocks with new types for testing

--- src/mocks/user/userMocks.ts
+++ src/mocks/user/userMocks.ts
@@ -1,27 +1,34 @@
-import { UserStateStructure, UserTokenStructure } from "../../store/user/types";
+import {
+  UserFavoritesStructure,
+  UserTokenStructure,
+} from "../../store/user/types";
 import { UserCredentials } from "../../types";
 
 export const decodedTokenMock: Pick<UserTokenStructure, "id"> = {
   id: "652a67fb1ae419782596f55c",
 };
 
-export const userTokenDataMock: UserTokenStructure = {
+export const userTokenDataMock: UserFavoritesStructure = {
   id: "652a67fb1ae419782596f55c",
   token:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTJhNjdmYjFhZTQxOTc4MjU5NmY1NWMiLCJuYW1lIjoiMDExIiwiaWF0IjoxNjk3NDU0Njg1LCJleHAiOjE2OTgwNTk0ODV9.NK8Uah881IzrDuYMHS5w-44EElIYPtMQ39rs9KWCNOc",
+  isLogged: true,
+  favoriteCars: ["6030b5d7b680ca87b2a1bf10"],
 };
 
-export const userInitialStateMock: UserStateStructure = {
+export const userInitialStateMock: UserFavoritesStructure = {
   id: "",
   token: "",
   isLogged: false,
+  favoriteCars: [],
 };
 
-export const userLoggedStateMock: UserStateStructure = {
+export const userLoggedStateMock: UserFavoritesStructure = {
   id: "1",
   token:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTJhNjdmYjFhZTQxOTc4MjU5NmY1NWMiLCJuYW1lIjoiMDExIiwiaWF0IjoxNjk3NDU0Njg1LCJleHAiOjE2OTgwNTk0ODV9.NK8Uah881IzrDuYMHS5w-44EElIYPtMQ39rs9KWCNOc",
   isLogged: true,
+  favoriteCars: ["6030b5d7b680ca87b2a1bf10"],
 };
 
 export const loginUserDataMock: UserCredentials = {
